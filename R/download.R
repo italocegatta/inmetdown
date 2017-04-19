9a00286859db9aeade1d97667551d96792442149
italocegatta <italocegatta@gmail.com>
2017-04-19 18:41:51 -0300

    fun aws_import

--- R/download.R
+++ R/download.R
@@ -1,71 +1,80 @@
 #' Download de dados INMET
-#' 
+#'
 #' Importa uma série de dados bruta do INMET de uma estação a partir do período
 #' especificado
-#' 
+#'
 #' @export
 #'
-aws_get_data <- function(id, start, end) {
-  # aws_get_data("A108", "18/04/2017", "18/04/2017")
+aws_import <- function(id, start, end, small = TRUE) {
+
   # debug
   # id = "A108"
   # start =  "18/04/2017"
   # end =  "18/04/2017"
   # load("R/sysdata.rda")
   # library(magrittr)
-  
+
   start <- check_date(start)
   end <- check_date(end)
-  
+
   session <- aws_get_url(id) %>%
     rvest::html_session()
-  
+
   img_cript <- session %>%
     rvest::html_nodes("img") %>%
     rvest::html_attr('src')
-  
+
   code_cript <- img_cript %>% stringr::str_extract("(?<==)(.*?)(?==)")
-  
+
   p1 <- key[key$code == stringr::str_sub(code_cript, 1, 3), "key"][[1]]
   p2 <- key[key$code == stringr::str_sub(code_cript, 4, 6), "key"][[1]]
-  
+
   form <- rvest::set_values(
     rvest::html_form(session)[[1]],
     `dtaini` = format(start, "%d/%m/%Y"),
     `dtafim` = format(end, "%d/%m/%Y"),
     `aleaNum` = paste0(p1, p2)
   )
-  
+
   data <- suppressMessages(rvest::submit_form(session, form))
-  
+
   x <- 0
   repeat {
     x <- x + 1
-    table <- aws_try_query()
+    table <- aws_try_query(data)
     if (!is.null(table)) break
     if (x > 2) break
   }
-  
+
   names(table) <- c(
-    "Data",
-    "Hora",
-    "Temperatura_ins",  "Temperatura_max", "Temperatura_min",
-    "Umidade_ins", "Umidade_max", "Umidade_min",
-    "Pto_Orvalho_ins", "Pto_Orvalho_max", "Pto_Orvalho_min",
-    "Pressao_int", "Pressao_max", "Pressao_min",
-    "Vento_vel", "Vento_dir", "Vento_raj",
-    "Radiacao",
-    "Chuva"
+    "data",
+    "hora",
+    "t_ins",  "t_max", "t_min",
+    "ur_ins", "ur_max", "ur_min",
+    "pto_orv_ins", "pto_orv_max", "pto_orv_min",
+    "pa_int", "pa_max", "pa_min",
+    "v_vel", "ento_dir", "v_raj",
+    "rad",
+    "p"
   )
-  
-  return(tibble::as_tibble(table))
+
+  z <- table %>%
+    tibble::as_tibble() %>%
+    dplyr::mutate_at(dplyr::vars(hora:rad), as.numeric) %>%
+    dplyr::mutate(data = lubridate::dmy(data))
+
+  if (small) {
+    z <- z %>%
+      dplyr::select(data, hora, t_max, t_min, ur_max, ur_min, rad, p)
+  }
+
+  return(z)
 }
 
 
-# Get URL's station 
+# Get URL's station
 aws_get_url <- function(id) {
-  df <- aws_stations() 
-  
+  df <- aws_stations()
   df[df$id %in% id, "url"][[1]]
 }
 
@@ -83,18 +92,18 @@ check_date <- function(x) {
       }
     }
   }
-  
+
   return(z)
 }
 
-aws_try_query <- function() {
+aws_try_query <- function(x) {
   tryCatch({
-    data %>%
+    x %>%
       rvest::html_nodes("table") %>%
       `[[`(6) %>%
       rvest::html_table(header = TRUE) %>%
       `[`(-1, )
-  }, 
+  },
   error=function(e) NULL,
   warning=function(w) NULL
   )

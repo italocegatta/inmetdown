928400289662860477304ff2876c337aae5e7678
italocegatta <italocegatta@gmail.com>
2017-04-20 11:25:58 -0300

    import vectorize

--- R/download.R
+++ R/download.R
@@ -1,3 +1,43 @@
+# Get URL's station
+aws_get_url <- function(id) {
+  df <- aws_stations()
+  df[df$id %in% id, "url"][[1]]
+}
+
+check_date <- function(x) {
+  if (!lubridate::is.Date(x)) {
+    test1 <- tryCatch(lubridate::dmy(x), warning=function(w) w)
+    if (!any((class(test1) == "warning") == TRUE)) {
+      z <- test1
+    } else {
+      test2 <- tryCatch(lubridate::ymd(x), warning=function(w) w)
+      if (lubridate::is.Date(test2)) {
+        z <- test2
+      } else {
+        stop("All formats failed to parse. No formats found.")
+      }
+    }
+  } else {
+    z <- x
+  }
+
+  return(z)
+}
+
+aws_try_query <- function(x) {
+  tryCatch({
+    x %>%
+      rvest::html_nodes("table") %>%
+      `[[`(6) %>%
+      rvest::html_table(header = TRUE) %>%
+      `[`(-1, )
+  },
+  error=function(e) NULL,
+  warning=function(w) NULL
+  )
+}
+
+
 #' Download de dados INMET
 #'
 #' Importa uma série de dados bruta do INMET de uma estação a partir do período
@@ -5,12 +45,20 @@
 #'
 #' @export
 #'
+
 aws_import <- function(id, start, end, small = TRUE) {
+  purrr::map_df(
+    id, ~import(.x, start = start, end = end, small = small)
+  )
+}
+
+import <- function(id, start, end, small) {
 
   # debug
+  # "A108","16/02/17", "17/04/17"
   # id = "A108"
-  # start =  "18/04/2017"
-  # end =  "18/04/2017"
+  # start =  as.Date("2017-02-16")
+  # end =  as.Date("2017-04-17")
   # load("R/sysdata.rda")
   # library(magrittr)
 
@@ -46,6 +94,14 @@ aws_import <- function(id, start, end, small = TRUE) {
     if (x > 2) break
   }
 
+  rm(x)
+
+  if (is.null(table)) {
+    table <- as.data.frame(matrix(NA_real_, nrow = 2, ncol = 19))
+    table[, 1] <- c(start, end)
+    table[, 2] <- c(0, 23)
+  }
+
   names(table) <- c(
     "data",
     "hora",
@@ -60,51 +116,26 @@ aws_import <- function(id, start, end, small = TRUE) {
 
   z <- table %>%
     tibble::as_tibble() %>%
-    dplyr::mutate_at(dplyr::vars(hora:rad), as.numeric) %>%
-    dplyr::mutate(data = lubridate::dmy(data))
+    dplyr::mutate_at(dplyr::vars(hora:p), as.double) %>%
+    dplyr::mutate(
+      data = if (is.character(data)) {
+          lubridate::dmy_hms(paste(data, paste0(hora, ":0:0")))
+        } else {
+          lubridate::ymd_hms(paste(data, paste0(hora, ":0:0")))
+        }
+      ,
+      rad = ifelse(rad < 0, 0, rad) / 1000
+    ) %>%
+    padr::pad() %>%
+    dplyr::mutate(id = id) %>%
+    dplyr::select(id, dplyr::everything(), -hora)
 
   if (small) {
     z <- z %>%
-      dplyr::select(data, hora, t_max, t_min, ur_max, ur_min, rad, p)
+      dplyr::select(id, data, t_max, t_min, ur_max, ur_min, rad, p)
   }
 
   return(z)
 }
 
 
-# Get URL's station
-aws_get_url <- function(id) {
-  df <- aws_stations()
-  df[df$id %in% id, "url"][[1]]
-}
-
-check_date <- function(x) {
-  if (!lubridate::is.Date(x)) {
-    test1 <- tryCatch(lubridate::dmy(x), warning=function(w) w)
-    if (!any((class(test1) == "warning") == TRUE)) {
-      z <- test1
-    } else {
-      test2 <- tryCatch(lubridate::ymd(x), warning=function(w) w)
-      if (lubridate::is.Date(test2)) {
-        z <- test2
-      } else {
-        stop("All formats failed to parse. No formats found.")
-      }
-    }
-  }
-
-  return(z)
-}
-
-aws_try_query <- function(x) {
-  tryCatch({
-    x %>%
-      rvest::html_nodes("table") %>%
-      `[[`(6) %>%
-      rvest::html_table(header = TRUE) %>%
-      `[`(-1, )
-  },
-  error=function(e) NULL,
-  warning=function(w) NULL
-  )
-}

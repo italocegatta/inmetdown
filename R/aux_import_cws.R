6045d57c514fbdf273589a4001136e8016b98d1f
italocegatta <italocegatta@gmail.com>
2018-03-27 09:06:15 -0300

    repeat request cws_import

--- R/aux_import_cws.R
+++ R/aux_import_cws.R
@@ -9,25 +9,27 @@ get_table_bdmep <- function(id, start, end, n_row, token) {
     "http://www.inmet.gov.br/projetos/rede/pesquisa/gera_serie_txt.php?&mRelEstacao={id}&btnProcesso=serie&mRelDtInicio={start_f}&mRelDtFim={end_f}&mAtributos=,,1,1,,,,,,1,1,,,,1,1,"
   )
 
-  txt <- get_txt_bdmep(con, param, token)
+  txt <- try(get_txt_bdmep(con, param, token), silent = TRUE)
 
   aux_error <- stringr::str_detect(txt, "Não existem dados disponiveis da estação")
 
-  table <- try(read.csv(text = txt, header = T, sep = ";"), silent = TRUE)
+  if (inherits(txt, "try-error") | aux_error) {
 
+    table <- table_if_error(id, start, end, n_row)
+    return(dplyr::as_tibble(table))
 
-  if (inherits(table, "try-error") | aux_error) {
-    table <- as.data.frame(matrix(NA_real_, nrow = n_row, ncol = 9))
-    table[ , 1] <- id
-    table[ , 2] <- rep(seq(start, end, by = "day"), each = 2)
-    table[ , 3] <- c(0, 12) * 100
-
-    if (end == Sys.Date()) {
-      table <- table[-nrow(table), ]
-    }
   } else {
-    table$X <- NULL
 
+    table <- try(read.csv(text = txt, header = T, sep = ";"), silent = TRUE)
+
+    if (inherits(txt, "try-error") | aux_error) {
+
+      table <- table_if_error(id, start, end, n_row)
+      return(dplyr::as_tibble(table))
+
+    }
+
+    table$X <- NULL
     table <- tibble::as_tibble(table)
   }
 
@@ -45,7 +47,22 @@ con_bdmep <- function() {
     mSenha = "qs7jsxwr"
   )
 
-  suppressMessages(rvest::submit_form(session, form))
+  x <- 0
+  repeat {
+    data <- tryCatch(
+      {
+        suppressMessages(rvest::submit_form(session, form))
+      },
+      error=function(e) NULL,
+      warning=function(w) NULL
+    )
+
+    x <- x + 1
+    if (!is.null(data)) break
+    if (x > 15) break
+  }
+
+  data
 }
 
 get_txt_bdmep <- function(con, param, token) {
@@ -53,4 +70,16 @@ get_txt_bdmep <- function(con, param, token) {
     rvest::html_nodes(xpath = "//pre") %>%
     rvest::html_text() %>%
     gsub(token, "", .)
+}
+
+table_if_error <- function(id, start, end, n_row) {
+  table <- as.data.frame(matrix(NA_real_, nrow = n_row, ncol = 9))
+  table[ , 1] <- id
+  table[ , 2] <- rep(seq(start, end, by = "day"), each = 2)
+  table[ , 3] <- c(0, 12) * 100
+
+  if (end == Sys.Date()) {
+    table <- table[-nrow(table), ]
+  }
+  table
 }
